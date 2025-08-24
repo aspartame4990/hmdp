7e187c8ca7a89d88d361da12cb2169508ead5a5f
gsc <g18603914990@foxmail.com>
2025-08-24 15:11:14 +0200

    finish P95 - 统计

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -150,4 +150,20 @@ class HmDianPingApplicationTests {
             redisTemplate.opsForGeo().add(key,locations);
         }
     }
+
+
+    @Test
+    public void testHyperLogLog(){
+        String[] values=new String[1000];
+        int j=0;
+        for (int i = 0; i < 1000000; i++) {
+            j=i%1000;
+            values[j]="user_"+i;
+            if (j==999){
+                redisTemplate.opsForHyperLogLog().add("hl1",values);
+            }
+        }
+        Long hl1 = redisTemplate.opsForHyperLogLog().size("hl1");
+        System.out.println(hl1);
+    }
 }
